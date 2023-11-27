24c60cae7ee78aca8ed8567eeef661f0a9e61a33
350444535 <150150007+350444535@users.noreply.github.com>
2023-11-27 08:18:13 -0500

    Add files via upload

--- MyWorld.java
+++ MyWorld.java
@@ -1,14 +1,18 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * The World, All in one frame
+ * how depressing
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Nicholas Kulybanycz) 
+ * @version (World Version I)
  */
 public class MyWorld extends World
 {
 
+    public int score = 0;
+    Label scoreLabel;
+    
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -16,6 +20,38 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1);
+        super(600, 400, 1, false); 
+
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 350);
+        
+        scoreLabel = new Label(0, 80);
+        addObject(scoreLabel, 50, 50);
+        
+        createApple();
+    }
+    
+    public void gameOver()
+    {
+        Label gameOverLabel = new Label("Game Over", 100);
+        addObject(gameOverLabel, 300, 200);
+    }
+    
+    public void increaseScore()
+    {
+        score++;
+        scoreLabel.setValue(score);
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    public void createApple()
+    {
+        Apple apple = new Apple();
+        int x = Greenfoot.getRandomNumber(600);
+        
+        addObject(apple, x, 0);
     }
 }
